906ca91f76276d2d8e5c5941479e0839f08458eb
Yamboy1 <yamboyd1@gmail.com>
2020-11-28 12:27:20 +1300

    Update examples

--- mod.ts
+++ mod.ts
@@ -3,5 +3,5 @@ import { operations } from "./operations.ts";
 
 const calculator = new Calculator(operations);
 
-console.log(calculator.calculate("4 2 +"));
-console.log(calculator.calculate("4 2 21 - *"));
+console.log(calculator.calculate("1 2 +"));
+console.log(calculator.calculate("3 4 5 * -"));
